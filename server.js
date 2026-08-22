26548bf078292bc1633b0c95c561068bf8ccc30f
Anuj <anuj.officialid@gmail.com>
2026-08-22 18:16:46 +0530

    fix server

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const jsonServer= require("json-server");
+const server = jsonServer.create();
+const router= jsonServer.router("db.json")
+const middleware= jsonServer.defaults()
+
+server.use(middleware);
+server.use(router)
+
+const port = process.env.PORT||3000
+server.listen(port,"0.0.0.0",()=>{
+  console.log("json-server running")
+})
